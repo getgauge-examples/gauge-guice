d9551da2ded7f646b2b7ecfd77ab3975b39686d1
Soumya <sswaroop@thoughtworks.com>
2017-07-25 08:18:34 +0530

    using the ClassInitializer

--- src/test/java/GuiceInjection.java
+++ src/test/java/GuiceInjection.java
@@ -1,12 +1,17 @@
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Singleton;
+import com.thoughtworks.gauge.BeforeStep;
 import com.thoughtworks.gauge.BeforeSuite;
+import com.thoughtworks.gauge.ClassInitializer;
 
-public class GuiceInjection {
+public class GuiceInjection implements ClassInitializer{
     public static Injector injector;
 
-    @BeforeSuite
-    public void registerGuice(){
+    @Override
+    public Object initialize(Class<?> aClass) throws Exception {
         injector = Guice.createInjector(new GuiceBindings());
+        return injector.getInstance(aClass);
     }
 }
--- src/test/java/PaymentService.java
+++ src/test/java/PaymentService.java
@@ -1,15 +1,30 @@
-import com.google.inject.Inject;
+import com.thoughtworks.gauge.Step;
+
+import javax.inject.Inject;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class PaymentService {
-    private final DebitCardProcessor processor;
+    private DebitCardProcessor processor;
 
     @Inject
-    PaymentService(DebitCardProcessor processor) {
+    public PaymentService(DebitCardProcessor processor) {
         this.processor = processor;
     }
 
-    public Receipt payAmount(int rupees) {
-        return this.processor.charge(rupees);
+    @Step("Pay amount <rupees> rupees")
+    public void payMoney(String amount) {
+        int rupees = Integer.parseInt(amount);
+        Receipt receipt = this.processor.charge(rupees);
+
+        assertEquals(receipt.getPaymentStatus(), ReceiptStatus.SUCCESS);
     }
 
+    @Step("Fail to pay <100> rupees")
+    public void failurePayment(String amount) {
+        int rupees = Integer.parseInt(amount);
+        Receipt receipt = this.processor.charge(rupees);
+
+        assertEquals(receipt.getPaymentStatus(), ReceiptStatus.FAILURE);
+    }
 }
